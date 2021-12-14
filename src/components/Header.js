1a38a32968b458048607cd7186ff1b16b4df2076
Dalton Otineru <obey.giant@icloud.com>
2021-12-13 23:24:32 -0800

    navbar responsive, hero responsive on mobile but not tablet. both need to be styled, but are laid out

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,66 @@ import ReactDOM from "react-dom";
 
 export const Header = () => {
   return (
-    <header id="header-container">
-      <div className="logo">SODA BAR</div>
-      <nav>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Menu</a>
-        <a href="#">Live Shows</a>
-        <a href="#">Contact</a>
-      </nav>
-    </header>
+    <nav
+      className="navbar navbar-expand-lg navbar-light sticky-top"
+      id="navbar"
+    >
+      <div className="container navbar-header align-center text-center">
+        <a className="navbar-brand align-items-center" href="#">
+          <span className="soda">SODA BAR</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto align-items-center">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Menu
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Live Shows
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    // <header id="header-container">
+    //   <div classNameName="logo">SODA BAR</div>
+    //   <nav>
+    //     <a href="#">Home</a>
+    //     <a href="#">About</a>
+    //     <a href="#">Menu</a>
+    //     <a href="#">Live Shows</a>
+    //     <a href="#">Contact</a>
+    //   </nav>
+    // </header>
   );
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,8 +7,8 @@ export const Hero = () => {
         <span className="border">
           <div className="inner">
             <span className="est">EST. | 2011</span>
-            <h1>SODA</h1>
-            <h1>BAR</h1>
+            <h1 id="soda-hero">SODA</h1>
+            <h1 id="bar-hero">BAR</h1>
             <span className="address">3615 El Cajon Blvd.</span>
             <span className="address">San Diego, CA 92104</span>
             <span className="hours">Open from 5PM - 2AM</span>
